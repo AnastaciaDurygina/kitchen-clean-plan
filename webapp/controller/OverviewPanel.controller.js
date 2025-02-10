b3d2ad0c3c934751bb3b36736b5d8d1eb53d088e
Anastasiia Durygina <anastasiia.durygina@systema.com>
2025-02-10 23:32:33 +0100

    Initial commit

---
+++ webapp/controller/OverviewPanel.controller.js
@@ -0,0 +1,88 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/m/Token"],
+  (Controller, MessageToast, Token) => {
+    "use strict";
+
+    return Controller.extend(
+      "ui5.kitchen-clean-plan.controller.OverviewPanel",
+      {
+        onNewEntry() {
+          // read msg from i18n model
+          const oBundle = this.getView().getModel("i18n").getResourceBundle();
+          const sRecipient = this.getView()
+            .getModel()
+            .getProperty("/recipient/name");
+          const sMsg = oBundle.getText("filterField", [sRecipient]);
+
+          // show message
+          MessageToast.show(sMsg);
+        },
+
+        onSearch: function () {
+          //TODO: funktionalität des Filters einfügen
+          sap.m.MessageToast.show("Search icon clicked!");
+        },
+
+        async onOpenDialog() {
+          // create dialog lazily
+          this.oDialog ??= await this.loadFragment({
+            name: "ui5.kitchen-clean-plan.view.NewEntryDialog",
+          });
+
+          this.oDialog.open();
+        },
+
+        onInit() {
+          var oMultiInput = this.getView().byId("taskInput");
+
+          if (oMultiInput) {
+            // tokenValidator aus Component.js verwenden
+            var oComponent = this.getOwnerComponent();
+            oMultiInput.addValidator(
+              oComponent.tokenValidator.bind(oComponent)
+            );
+          }
+        },
+
+        //Neue Tätigkeiten als Token hinzufügen
+        onTaskAdd(oEvent) {
+          var oMultiInput = this.getView().byId("taskInput");
+          // Den eingegebenen Wert aus dem Event holen und Leerzeichen entfernen
+          var sValue = oEvent.getParameter("value")?.trim();
+
+          if (!sValue) {
+            return; // Keine leeren Tokens hinzufügen
+          }
+
+          // Prüfen, ob das Token bereits existiert (doppelte vermeiden)
+          var aTokens = oMultiInput.getTokens();
+          var bExists = aTokens.some((token) => token.getText() === sValue);
+
+          // Falls das Token noch nicht existiert, wird es hinzugefügt
+          if (!bExists) {
+            oMultiInput.addToken(new Token({ key: sValue, text: sValue }));
+          }
+
+          // Eingabefeld leeren
+          oMultiInput.setValue("");
+        },
+
+        //Icon (valueHelp) wird geklickt -> alle Tokens werden gelöscht
+        onTaskDelete() {
+          this.getView().byId("taskInput").removeAllTokens();
+        },
+
+        onCreateDialog() {
+          // TODO: Logik für Erstellen neues Eintrags
+          this.byId("NewEntryDialog").close(); //may be change function
+        },
+
+        onCloseDialog() {
+          // note: We don't need to chain to the pDialog promise, since this event handler
+          // is only called from within the loaded dialog itself.
+          this.byId("NewEntryDialog").close();
+        },
+      }
+    );
+  }
+);
